92374dd6b11c23a68e7df3decaaf9444ee2f3f18
martinlabjkpsm-cpu <martin.lab.jkpsm@gmail.com>
2026-09-24 15:25:11 +0000

    finalizacion pagina

--- src/components/Atoms/Imagen.jsx
+++ src/components/Atoms/Imagen.jsx
@@ -1,4 +1,11 @@
-const Imagen =({src, alt, className = ""}) => (
-    <img src={src} className={className} />
-);
+const Imagen = ({ src, alt = "Imagen", className = "logo" }) => {
+  return (
+    <img 
+      src={src} 
+      alt={alt} 
+      className={className} 
+    />
+  );
+};
+
 export default Imagen;
--- src/components/Organisms/Encabezado.jsx
+++ src/components/Organisms/Encabezado.jsx
@@ -1,5 +1,3 @@
-
-import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import Presentacion from '../Molecules/Presentacion'; 
